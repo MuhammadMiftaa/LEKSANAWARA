2b3f2794cdeef46c451e7e46b511f763be3aebde
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-12-07 06:57:30 +0700

    router setup🎲

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"os"
+	"smart-home-energy-management-server/interface/http/router"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Println("Error loading .env file")
+		}
+	}
+
+	port := os.Getenv("PORT")
+
+	r := router.SetupRouter()
+	r.Run(":" + port)
 }
---
+++ server/interface/http/router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"smart-home-energy-management-server/interface/http/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.Use(middleware.CORSMiddleware())
+
+	router.GET("test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	// v1 := router.Group("/v1")
+
+	return router
+}

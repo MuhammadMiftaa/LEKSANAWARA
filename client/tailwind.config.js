3261c159d3108d36d6a857d280793601bcdbbf74
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-12-10 15:30:42 +0700

    frontend init (react typescript vite w/tailwindcss+shadcn)🎲

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+      fontFamily: {
+        inter: ["Inter"],
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+};

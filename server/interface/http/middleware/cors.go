38baaeb4afbfb0ae515505a526068bbb95ee8d54
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-12-28 12:09:35 +0700

    mini update⚡

--- server/interface/http/middleware/cors.go
+++ server/interface/http/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173" , "https://leksanawara.vercel.app"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173" , "https://leksanawara.vercel.app", "https://leksanawara-git-production-muhammad-miftas-projects-f0b46e0b.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
